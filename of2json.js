92ca24fbc1fcc5ae8659b605f2cd5e918c481561
magrawala <maneesh@eecs.berkeley.edu>
2016-12-04 15:03:09 -0800

    added in facepts bbox

--- of2json.js
+++ of2json.js
@@ -45,91 +45,147 @@ for (var i in ptsFiles) {
 					     landmarkPtsEndIdx-1);
     var lPts = landmarkPts.split(/\n/);
     if (lPts.length != 68) {throw("Wrong landmarkPts length");}
+
+    //Save minBBoxPt and maxBBoxPt 
+    var minPt = {face_minBBox: {x: 100000, y:100000}};
+    var maxPt = {face_maxBBox: {x: 0, y:0}};
+    
     //Based on https://github.com/TadasBaltrusaitis/OpenFace/wiki/Output-Format
-    //Pts 0 - 16 are on the face contour, 17-21 are left eyebrow,
-    //22-26 are right eyebrow, 27-30 are nose centerline, 31 - 35 are
-    //nose bottom, 36-41 are left eye, 42-47 are right eye,
-    //48-53 are upper lip top, 54-59 are lower lip bottom,
-    //60-63 are upper lip bottom, 64-67 are lower lip bottom.
+    //Pts 0 - 16 are on the face contour 
     for (var idx = 0; idx < 17; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "contour_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 17-21 are left eyebrow
     for (var idx = 17; idx < 22; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "left_eyebrow_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 22-26 are right eyebrow,
     for (var idx = 22; idx < 27; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "right_eyebrow_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 27-30 are nose centerline,
     for (var idx = 27; idx < 31; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "nose_centerline_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 31 - 35 are nose bottom
     for (var idx = 31; idx < 36; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "nose_bottom_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 36-41 are left eye
     for (var idx = 36; idx < 42; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "left_eye_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 42-47 are right eye
     for (var idx = 42; idx < 48; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "right_eye_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 48-53 are upper lip top 
     for (var idx = 48; idx < 54; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "mouth_upper_lip_top_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 54-59 are lower lip bottom
     for (var idx = 54; idx < 60; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "mouth_lower_lip_bottom_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 60-63 are upper lip bottom 
     for (var idx = 60; idx < 64; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "mouth_upper_lip_bottom_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
+    //Pts 64-67 are lower lip bottom
     for (var idx = 64; idx < 68; idx++) {
 	var currPt = lPts[idx].split(" ");
 	var ptName = "mouth_lower_lip_top_" + idx;
 	outLPts.landmark[ptName] = new Object();
 	outLPts.landmark[ptName].x = currPt[0];
 	outLPts.landmark[ptName].y = currPt[1];
+	if (currPt[0] < minPt.face_minBBox.x) minPt.face_minBBox.x = currPt[0];
+	if (currPt[1] < minPt.face_minBBox.y) minPt.face_minBBox.y = currPt[1];
+	if (currPt[0] > maxPt.face_maxBBox.x) maxPt.face_maxBBox.x = currPt[0];
+	if (currPt[1] > maxPt.face_maxBBox.y) maxPt.face_maxBBox.y = currPt[1];
     }
-//    console.log(outLPts);
     currFaceObj.landmark = outLPts.landmark; 
+    currFaceObj.face_minBBox = minPt.face_minBBox;
+    currFaceObj.face_maxBBox = maxPt.face_maxBBox;
     
     //Construct pose object
     var outPose = {pose: {eul_x:0, eul_y:0, eul_z:0}};
@@ -213,7 +269,7 @@ for (var i in ptsFiles) {
 	//Write out the old result to a JSON file
 	var outFileName = path + prevFrameRootName + "_landmarks.json";
 	console.log(outFileName);
-	fs.writeFileSync(outFileName, JSON.stringify(result,null,"   "));
+	fs.writeFileSync(outFileName, JSON.stringify(result, null, "   "));
 	
 	//Empty the result and update prevFrameRootName
 	result = {result: []};
@@ -226,7 +282,7 @@ for (var i in ptsFiles) {
 //Write out the old result to a JSON file
 var outFileName = path + prevFrameRootName + "_landmarks.json";
 console.log(outFileName);
-    fs.writeFileSync(outFileName, JSON.stringify(result,null,"   "));
+fs.writeFileSync(outFileName, JSON.stringify(result, null, "   "));
 
 
 
